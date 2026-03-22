f4547d432f69cf3250f34000bb6c5f2b200bb0e0
ab sam <sam749598@gmail.com>
2026-03-22 22:02:21 +0600

    update cors for frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,7 +10,13 @@ const app = express()
 
 // ─── Global Middlewares ──────//
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://auth-full-frontend.vercel.app/",  // 
+  ],
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
